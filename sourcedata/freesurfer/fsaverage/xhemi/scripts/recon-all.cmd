4804f3539dfb5a22f82339da6e34d4e340e78929
jbwexler <jbwexler@tutanota.com>
2023-06-01 15:59:00 -0500

    make ready for publish

--- sourcedata/freesurfer/fsaverage/xhemi/scripts/recon-all.cmd
+++ sourcedata/freesurfer/fsaverage/xhemi/scripts/recon-all.cmd
@@ -1,16 +1,1 @@
-
-
-#---------------------------------
-# New invocation of recon-all Fri Nov 13 11:35:23 EST 2015 
-#--------------------------------------------
-#@# Talairach Fri Nov 13 11:35:32 EST 2015
-
- mri_nu_correct.mni --no-rescale --i orig.mgz --o orig_nu.mgz --n 1 --proto-iters 1000 --distance 200 --stop 1e-4 --shrink 2 
-
-
- talairach_avi --i orig_nu.mgz --xfm transforms/talairach.auto.xfm 
-
-talairach_avi log file is transforms/talairach_avi.log...
-
- cp transforms/talairach.auto.xfm transforms/talairach.xfm 
-
+../../../../../.git/annex/objects/8Z/mK/MD5E-s504--4632648fd5945e9704c010e99614d3d6.cmd/MD5E-s504--4632648fd5945e9704c010e99614d3d6.cmd
